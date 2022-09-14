a597fd6584b2c2d2ca8ba171e26b7a78594e614d
Dynkina Mariia <mashenkamelon@icloud.com>
2022-09-14 13:14:43 +0300

    added brain-gcd.js

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import { gameGcd } from '../src/games/gcd-logic.js';
+
+
+gameGcd();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import readlineSync from "readline-sync";
-
-export const run = () => {
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question("May I have your name? ");
-console.log(`Hello, ${userName}!`);
-}
-
 export const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
+
+
